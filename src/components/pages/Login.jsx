faebf7a12e578cef3897217cb1cefc270f0dc739
Victor <Victor@gmail.com>
2024-07-26 10:19:07 +0100

    url

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState, useContext } from "react"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Navigate } from "react-router-dom"
 import EcomContext from "../../context/EcomContext"
 import AuthContext from "../../context/AuthContext"
 import useLocalStorage from "../../Hooks/useLocalStorage"
